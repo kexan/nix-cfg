ca890ba178d88a72f1158863e1eeb09d1bcbee0f
Sergei Nikitin <kexa76@gmail.com>
2025-11-21 13:16:45 +0300

    Zen Browser: settings added

--- hm-modules/programs/zen-browser.nix
+++ hm-modules/programs/zen-browser.nix
@@ -1,8 +1,26 @@
-{ config, pkgs, inputs, ... }:
+{ inputs, ... }:
 
 {
   imports = [ inputs.zen-browser.homeModules.beta ];
-
-  #TODO: описать конфиг zen
-  programs.zen-browser.enable = true;
+  programs.zen-browser = {
+    enable = true;
+    profiles.kexan = {
+      settings = {
+        "browser.aboutConfig.showWarning" = false;
+        "browser.search.region" = "RU";
+        "browser.search.suggest.enabled" = true;
+        "browser.urlbar.placeholderName" = "Google";
+        "browser.urlbar.placeholderName.private" = "Google";
+        "browser.translations.automaticallyPopup" = false;
+        "intl.locale.requested" = "ru,en-US";
+        "intl.regional_prefs.use_os_locales" = true;
+        "intl.accept_languages" = "ru-RU, ru, en-US, en";
+        "font.cjk_pref_fallback_order" = "ja,zh-cn,zh-hk,zh-tw,ko";
+        "font.name.monospace.ja" = "Noto Sans Mono CJK JP";
+        "font.name.sans-serif.ja" = "Noto Serif CJK JP";
+        "font.name.serif.ja" = "Noto Sans CJK JP";
+        "zen.welcome-screen.seen" = true;
+      };
+    };
+  };
 }
--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -8,6 +8,4 @@
     homeDirectory = "/home/kexan";
     stateVersion = "25.05";
   };
-
-  xdg.configFile."fontconfig/conf.d/10-hm-fonts.conf".force = true;
 }
