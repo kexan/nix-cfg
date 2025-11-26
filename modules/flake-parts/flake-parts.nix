67ed17e339d2a87c3d8bf26663d4fc5e9ad4d8b2
Sergei Nikitin <kexa76@gmail.com>
2025-11-26 13:14:49 +0300

    Flake-parts module clean-up

--- modules/flake-parts/flake-parts.nix
+++ modules/flake-parts/flake-parts.nix
@@ -1,5 +1,9 @@
-{ inputs, ... }:
+{ lib, inputs, ... }:
 
 {
   imports = [ inputs.flake-parts.flakeModules.modules ];
+
+  options.flake.meta = lib.mkOption {
+    type = with lib.types; lazyAttrsOf anything;
+  };
 }
