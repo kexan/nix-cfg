e4103702a60b6d02c74f7608918a605dc7466f44
Sergei Nikitin <kexa76@gmail.com>
2025-12-03 13:34:33 +0300

    Gaming: make portproton independent of flatpak-module

--- modules/gaming/portproton.nix
+++ modules/gaming/portproton.nix
@@ -1,11 +1,25 @@
 {
   flake.modules = {
-    nixos.gaming = {
-      services.flatpak = {
-        packages = [
-          "ru.linux_gaming.PortProton"
+    nixos.gaming =
+      {
+        inputs,
+        config,
+        lib,
+        ...
+      }:
+      {
+        imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
+
+        services.flatpak = {
+          enable = lib.mkDefault true;
+          packages = lib.mkIf config.services.flatpak.enable [
+            "ru.linux_gaming.PortProton"
+          ];
+        };
+
+        warnings = lib.mkIf (!config.services.flatpak.enable) [
+          "Gaming: Flatpak is disabled. PortProton will not be installed."
         ];
       };
-    };
   };
 }
