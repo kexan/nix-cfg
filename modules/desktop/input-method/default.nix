876726ec845f782b3b8cf6cc968efa7f4c33071b
Sergey Nikitin <kexa76@gmail.com>
2025-12-21 15:38:52 +0300

    refactor: configure Mozc toggle with Ctrl+Alt+Space, keep Caps Lock for EN/RU

--- modules/desktop/input-method/default.nix
+++ modules/desktop/input-method/default.nix
@@ -29,11 +29,16 @@
       i18n.inputMethod.fcitx5.settings = {
         # Глобальные настройки
         globalOptions = {
+          # Ctrl+Alt+Space для включения/выключения Mozc
           "Hotkey/TriggerKeys" = {
-            "0" = "Control+space";
+            "0" = "Control+Alt+space";
           };
-          "Hotkey/EnumerateForwardKeys" = {
-            "0" = "Alt+Shift_L";
+          # Отключаем стандартное переключение между всеми методами
+          "Hotkey/EnumerateForwardKeys" = { };
+          "Hotkey/EnumerateBackwardKeys" = { };
+          # Деактивировать метод ввода той же клавишей
+          "Hotkey/DeactivateKeys" = {
+            "0" = "Control+Alt+space";
           };
         };
 
@@ -42,16 +47,19 @@
           "Groups/0" = {
             Name = "Default";
             "Default Layout" = "us";
+            # По умолчанию используем клавиатурные раскладки (без IME)
             DefaultIM = "keyboard-us";
           };
+          # Keyboard layouts для переключения через Caps Lock (настроено в KDE)
           "Groups/0/Items/0" = {
             Name = "keyboard-us";
           };
           "Groups/0/Items/1" = {
-            Name = "mozc";
-          };
-          "Groups/0/Items/2" = {
             Name = "keyboard-ru";
+          };
+          # Mozc активируется отдельно через Ctrl+Alt+Space
+          "Groups/0/Items/2" = {
+            Name = "mozc";
           };
           GroupOrder = {
             "0" = "Default";
