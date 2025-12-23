1deed7d3001c6ea23ca23f19a5423b152861d8fd
Sergei Nikitin <kexa76@gmail.com>
2025-12-23 09:34:52 +0300

    Zen: fix jp fonts

--- modules/desktop/web-browsers/zen.nix
+++ modules/desktop/web-browsers/zen.nix
@@ -19,8 +19,8 @@
             "intl.accept_languages" = "ru-RU, ru, en-US, en";
             "font.cjk_pref_fallback_order" = "ja,zh-cn,zh-hk,zh-tw,ko";
             "font.name.monospace.ja" = "Noto Sans Mono CJK JP";
-            "font.name.sans-serif.ja" = "Noto Serif CJK JP";
-            "font.name.serif.ja" = "Noto Sans CJK JP";
+            "font.name.sans-serif.ja" = "Noto Sans CJK JP";
+            "font.name.serif.ja" = "Noto Serif CJK JP";
             "font.name-list.emoji" = "JoyPixels, Noto Color Emoji, Twemoji Mozilla";
             "zen.welcome-screen.seen" = true;
           };
