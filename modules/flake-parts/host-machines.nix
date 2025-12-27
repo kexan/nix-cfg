1b99c7d1bcad74cabd6c446b0bad4e2d6bdb2488
Sergei Nikitin <kexa76@gmail.com>
2025-12-27 23:01:17 +0300

    Flake-parts: refactor host-machines func

--- modules/flake-parts/host-machines.nix
+++ modules/flake-parts/host-machines.nix
@@ -5,35 +5,29 @@
   ...
 }:
 let
-  prefix = "hosts/";
-  collectHostsModules = modules: lib.filterAttrs (name: _: lib.hasPrefix prefix name) modules;
+  hostsPrefix = "hosts/";
+  hostModules = lib.filterAttrs (name: _: lib.hasPrefix hostsPrefix name) config.flake.modules.nixos;
+  mkHost =
+    name: module:
+    let
+      hostName = lib.removePrefix hostsPrefix name;
+      specialArgs = {
+        inherit inputs;
+        hostConfig.name = hostName;
+      };
+    in
+    {
+      name = hostName;
+      value = inputs.nixpkgs.lib.nixosSystem {
+        inherit specialArgs;
+        modules = [
+          module
+          inputs.home-manager.nixosModules.home-manager
+          { home-manager.extraSpecialArgs = specialArgs; }
+        ];
+      };
+    };
 in
 {
-  flake.nixosConfigurations = lib.pipe (collectHostsModules config.flake.modules.nixos) [
-    (lib.mapAttrs' (
-      name: module:
-      let
-        hostName = lib.removePrefix prefix name;
-        specialArgs = {
-          inherit inputs;
-          hostConfig = {
-            name = hostName;
-          };
-        };
-      in
-      {
-        name = hostName;
-        value = inputs.nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          modules = [
-            module
-            inputs.home-manager.nixosModules.home-manager
-            {
-              home-manager.extraSpecialArgs = specialArgs;
-            }
-          ];
-        };
-      }
-    ))
-  ];
+  flake.nixosConfigurations = lib.mapAttrs' mkHost hostModules;
 }
