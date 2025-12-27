aeff2d5a0c4cd2611d16f9ee8be27cb6ce6f6eeb
Sergei Nikitin <kexa76@gmail.com>
2025-12-27 23:27:13 +0300

    Hosts: add redmibook host

---
+++ modules/hosts/redmibook/default.nix
@@ -0,0 +1,78 @@
+{
+  config,
+  ...
+}:
+
+{
+  flake.modules.nixos."hosts/redmibook" = {
+    imports =
+      with config.flake.modules.nixos;
+      [
+        base
+        desktop
+        plasma
+        sound
+        shell
+        openssh
+        virtualbox
+        flatpak
+        gaming
+        vpn
+        root
+        kexan
+      ]
+
+      # Specific Home-Manager modules
+      ++ [
+        {
+          home-manager.users.kexan = {
+            imports = with config.flake.modules.homeManager; [
+              ai
+              base
+              desktop
+              plasma
+              dev
+              messaging
+              shell
+              gaming
+              vpn
+            ];
+          };
+        }
+      ];
+
+    #TODO: ADD FACTER AND FILESYSTEMS!!!
+    # hardware.facter.reportPath = ./facter.json;
+    #
+    # fileSystems = {
+    #   "/" = {
+    #     device = "/dev/disk/by-uuid/0bc05461-e615-466d-bf53-6192417f74d4";
+    #     fsType = "ext4";
+    #   };
+    #
+    #   "/boot" = {
+    #     device = "/dev/disk/by-uuid/9865-7182";
+    #     fsType = "vfat";
+    #     options = [
+    #       "fmask=0077"
+    #       "dmask=0077"
+    #     ];
+    #   };
+    # };
+
+    zramSwap.enable = true;
+
+    networking = {
+      hosts = {
+        "192.168.1.100" = [
+          "linuxservice.test"
+          "yg.linuxservice.test"
+        ];
+        "192.168.1.101" = [
+          "yougile.local"
+          "yg.yougile.local"
+        ];
+      };
+    };
+  };
+}
