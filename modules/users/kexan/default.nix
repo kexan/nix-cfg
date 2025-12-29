74270729a038fed85a5ff251432b85a9f2681030
Sergei Nikitin <kexa76@gmail.com>
2025-12-29 16:23:41 +0300

    Users: make ssh key management more convenient

--- modules/users/kexan/default.nix
+++ modules/users/kexan/default.nix
@@ -1,20 +1,21 @@
-{
-  config,
-  ...
-}:
+{ config, ... }:
 
 {
   flake = {
-    meta.users = {
-      kexan = {
-        email = "kexa76@gmail.com";
-        name = "Sergei Nikitin";
-        username = "kexan";
-        authorizedKeys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPqSOM2HdLv6kEa1e6Mv82bGBYJ7MnD/LrDSaU6P6Nk5 kexan@MacBook-Air-Sergei.local"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINoEOEalFKRQplgze0K4HIFXUun2NcEUgb+ou/4UnKZB kexan@arch"
-        ];
+    meta.users.kexan = rec {
+      email = "kexa76@gmail.com";
+      name = "Sergei Nikitin";
+      username = "kexan";
+
+      sshKeys = {
+        default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINoEOEalFKRQplgze0K4HIFXUun2NcEUgb+ou/4UnKZB kexan@arch";
+        mac = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPqSOM2HdLv6kEa1e6Mv82bGBYJ7MnD/LrDSaU6P6Nk5 kexan@MacBook-Air-Sergei.local";
       };
+
+      authorizedKeys = [
+        sshKeys.mac
+        sshKeys.default
+      ];
     };
 
     modules.nixos.kexan = {
@@ -44,8 +45,7 @@
     };
 
     modules.homeManager.kexan = {
-      home.file.".ssh/id_ed25519.pub".text =
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINoEOEalFKRQplgze0K4HIFXUun2NcEUgb+ou/4UnKZB kexan@arch";
+      home.file.".ssh/id_ed25519.pub".text = config.flake.meta.users.kexan.sshKeys.default;
     };
   };
 }
