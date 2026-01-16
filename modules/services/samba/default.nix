00b57f6ac904e38a694d3ae603f551eb59287bf9
Sergei Nikitin <kexa76@gmail.com>
2026-01-16 22:28:49 +0300

    samba: add compatibility settings

--- modules/services/samba/default.nix
+++ modules/services/samba/default.nix
@@ -3,6 +3,24 @@
     services.samba = {
       enable = true;
       openFirewall = true;
+      settings = {
+        global = {
+          "server min protocol" = "SMB2_02";
+          "netbios name" = "nix-samba";
+          "server string" = "NixOS Samba Server";
+
+          "vfs objects" = "catia fruit streams_xattr";
+          "fruit:metadata" = "stream";
+          "fruit:model" = "MacSamba";
+          "fruit:nfs_aces" = "no";
+          "fruit:posix_rename" = "yes";
+          "fruit:veto_appledouble" = "no";
+          "fruit:wipe_intentionally_left_blank_rfork" = "yes";
+          "fruit:delete_empty_adfiles" = "yes";
+
+          "map to guest" = "Bad User";
+        };
+      };
     };
   };
 }
