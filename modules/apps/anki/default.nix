0ebb7c26873c08322b4dc48e36ce41382b5482d6
Sergei Nikitin <kexa76@gmail.com>
2026-01-19 18:31:42 +0300

    anki: add addons and autostart

--- modules/apps/anki/default.nix
+++ modules/apps/anki/default.nix
@@ -8,8 +8,28 @@
         config,
         ...
       }:
+
       let
         sopsEnabled = config.sops.enable or false;
+
+        minimizeToTray = pkgs.anki-utils.buildAnkiAddon (finalAttrs: {
+          pname = "minimize-to-tray";
+          version = "2.1.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "simgunz";
+            repo = "anki21-addons_minimize-to-tray";
+            rev = finalAttrs.version;
+            sparseCheckout = [ "src" ];
+            hash = "sha256-REoh97eUSqlkLvFkcsj2IHDFT3McnfBXlNz61o1omys=";
+          };
+          sourceRoot = "${finalAttrs.src.name}/src";
+
+          preInstall = ''
+            rm -f config.json
+
+            echo '{"hide_on_startup": true, "debug": false}' > config.json
+          '';
+        });
       in
       {
         imports = [ inputs.sops-nix.homeManagerModules.sops ];
@@ -18,12 +38,20 @@
           {
             programs.anki = {
               enable = true;
-              addons = [ pkgs.ankiAddons.anki-connect ];
+              addons = [
+                pkgs.ankiAddons.anki-connect
+                pkgs.ankiAddons.review-heatmap
+                minimizeToTray
+              ];
 
               language = "ru_RU";
               theme = "followSystem";
               style = "native";
             };
+
+            xdg.autostart.entries = [
+              "${pkgs.anki}/share/applications/anki.desktop"
+            ];
           }
 
           (lib.mkIf sopsEnabled {
