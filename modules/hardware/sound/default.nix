c73f15a2f694dc2c19894a5470d670874a77cb66
Sergei Nikitin <kexa76@gmail.com>
2026-02-23 01:20:29 +0300

    Update flake.lock and add PipeWire defaults

--- modules/hardware/sound/default.nix
+++ modules/hardware/sound/default.nix
@@ -6,6 +6,14 @@
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
+        extraConfig.pipewire = {
+          "context.properties" = {
+            "default.clock.rate" = 48000;
+            "default.clock.quantum" = 2048;
+            "default.clock.min-quantum" = 2048;
+            "default.clock.max-quantum" = 8192;
+          };
+        };
       };
     };
 
