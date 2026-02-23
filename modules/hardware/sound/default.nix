cdb52d8b51c9d1535fbddc97958530056e5fabc7
Sergei Nikitin <kexa76@gmail.com>
2026-02-23 20:47:58 +0300

    Configure PipeWire quantum to 1024

--- modules/hardware/sound/default.nix
+++ modules/hardware/sound/default.nix
@@ -9,8 +9,8 @@
         extraConfig.pipewire = {
           "context.properties" = {
             "default.clock.rate" = 48000;
-            "default.clock.quantum" = 2048;
-            "default.clock.min-quantum" = 2048;
+            "default.clock.quantum" = 1024;
+            "default.clock.min-quantum" = 1024;
             "default.clock.max-quantum" = 8192;
           };
         };
