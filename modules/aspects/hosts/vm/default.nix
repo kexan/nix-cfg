cc1e4507957f1ab59acc68d537d2eee018848cfb
Sergei Nikitin <kexa76@gmail.com>
2026-03-28 17:04:10 +0300

    wip

--- modules/aspects/hosts/vm/default.nix
+++ modules/aspects/hosts/vm/default.nix
@@ -9,34 +9,16 @@
       includes = with den.aspects; [
         base
         shell
-        openssh
         sops
         vpn
 
         desktop._.plasma
-        apps._.anki
         apps._.zen-browser
-        apps._.messaging
-        apps._.winbox
-        apps._.ocr
-
-        development._.zed
-        development._.lazyvim
-
-        gaming._.base
-        gaming._.emulation
 
         (hardware._.facter ./facter.json)
-        hardware._.corectrl
         hardware._.tuned
 
-        virtualisation._.virtualbox
-
         services._.openssh
-        services._.flatpak
-        services._.caddy
-        services._.jellyfin
-        services._.qbittorrent
         services._.tailscale
       ];
     };
